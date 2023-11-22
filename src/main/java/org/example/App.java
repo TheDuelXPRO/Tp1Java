e020cffd42ac5171b18bb8974457f31b6430f712
TECNOLOGIA <gabrielrodriguesconsentino@gmail.com>
2023-11-22 16:04:54 -0300

    Tp 1 Java

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,11 @@
+package org.example;
+import lombok.Data;
+
+@Data
+public class App 
+{
+    public void primeiroMetodo() {
+        System.out.println("Unico metodo aqui");
+        int x = 5;
+    }
+}
